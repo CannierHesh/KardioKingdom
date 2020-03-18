a8cccaf0caed676c349687ee57750b420a3c456b
HP <majintha.c@gmail.com>
2020-03-19 02:34:07 +0530

    second commit

--- app/src/main/java/com/example/gittest/test03.java
+++ app/src/main/java/com/example/gittest/test03.java
@@ -2,25 +2,30 @@ package com.example.gittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gittest.R;
+
+
+public class test03 extends AppCompatActivity {
     private Button Button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test03);
+
 
         Button = (Button)findViewById(R.id.button);
         Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,test03.class);
+                Intent intent = new Intent(test03.this, test04.class);
                 startActivity(intent);
-
             }
         });
     }
---
+++ app/src/main/java/com/example/gittest/test04.java
@@ -0,0 +1,16 @@
+package com.example.gittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gittest.R;
+
+public class test04 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test04);
+    }
+}
