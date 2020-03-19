b284b5da773c496e79e8ec0bda01d98cfd2f5374
Kardio Kingdom <babeysundara@gmail.com>
2020-03-19 12:50:36 +0530

    Press Commit

---
+++ app/src/main/java/com/example/kardiokingdom_binura/addexercises.java
@@ -0,0 +1,14 @@
+package com.example.kardiokingdom_binura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addexercises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addexercises);
+    }
+}
